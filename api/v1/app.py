b9d0b8330e6197a6cf0789477877614fb87f550f
saad-out <outsaad03@gmail.com>
2023-03-10 18:08:19 +0100

    add docstrings

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,64 @@
+"""
+This `app` module is the main entry for the TeamZone API Flask application version 1.
+
+It creates and configures a new Flask application, and registers various
+routes and error handlers.
+
+Routes:
+- `status`: Returns the status of the API.
+
+Error Handlers:
+- `404`: Handles page not found errors with a custom error page.
+
+
+Configuration:
+The application can be configured with the following environment variables:
+- `APP_SETTINGS`: The configuration class to use (default is `config.DevelopmentConfig`).
+- `DATABASE_URL`: The URL for the application's database (default is `sqlite:///data.db`).
+
+Usage:
+To start the application, simply run this module directly:
+
+    $ python app.py
+
+The application will be available at http://localhost:5001/.
+"""
 from flask import Flask, jsonify
 from flask_cors import CORS
 
+from api.v1.views import app_views
+
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
 CORS(app)
 
 
 @app.route('/status')
 def status():
+    """
+    Return a JSON object indicating the status of the application.
+
+    Returns:
+    A JSON object with a single key-value pair, where the key is "status"
+    and the value is "OK".
+    """
     return jsonify({"status": "OK"})
+
+@app.errorhandler(404)
+def not_found(error):
+    """
+    Handle a 404 error by returning a JSON error message.
+
+    Args:
+    error: The 404 error that occurred.
+
+    Returns:
+    A JSON object with a single key-value pair, where the key is "error"
+    and the value is "Not found". The response status code is 404.
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
---
+++ api/v1/views/__init__.py
@@ -0,0 +1,24 @@
+"""
+The `views` package contains Flask blueprints for various parts of the application.
+
+This package defines the `app_views` blueprint for the application's views.
+
+Blueprints:
+- `app_views`: A blueprint for the application's views.
+
+Usage:
+The blueprints defined in this package can be registered with the Flask application
+in the main entry point (`app.py`) as follows:
+
+    app.register_blueprint(app_views)
+
+This will register the `app_views` blueprint with the Flask application, making its routes
+available at the URL prefix `/api/v1`.
+
+See the Flask documentation for more information on using blueprints:
+http://flask.palletsprojects.com/en/2.1.x/blueprints/
+"""
+from flask import Blueprint
+
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
